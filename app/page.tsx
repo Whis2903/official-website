278fc9363296fc418840c9ef386acb6cb98e893f
vinayakfr <vinayak.sri@yahoo.com>
2024-10-14 16:45:50 +0530

    chore: fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Domain from "./components/Domains";
 import Footer from "./components/footer";
 import Hero from "./components/hero";
 import Navbar from "./components/Navbar";
-import NewJoin from "./components/newjoin";
 
 const HomePage: React.FC = () => {
   return (
