82c5b41fc8e850c58fd902a64293acd8cae4752c
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2024-10-14 17:11:48 +0530

    added logo to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,8 +5,7 @@ const Footer = () => {
     <footer className="text-center py-4 bg-white">
       <div className="grid grid-cols-3 place-content-center place-items-center w-full">
         <div className="h-[2px] w-[97%] bg-blue-500" />
-        {/* <img src="/logo.png" alt="Logo" className="w-60 h-30" /> */}
-        <h1 className="text-4xl font-semibold">Logo</h1>
+          <img src="/footer-logo.png" alt="Logo" className="w-32 h-18" /> 
         <div className="h-[2px] w-[97%] bg-blue-500" />
       </div>
 
