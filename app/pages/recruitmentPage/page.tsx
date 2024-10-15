ad2128e014ef358fced93066d3a57e6d88acdf32
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2024-10-16 00:17:17 +0530

    changes

--- app/pages/recruitmentPage/page.tsx
+++ app/pages/recruitmentPage/page.tsx
@@ -3,13 +3,12 @@ import React from "react";
 import RecruitmentPortal from "../../components/RecruitmentPortal";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/footer";
-
 const RecruitmentPage = () => {
   return (
-    <div className="bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen"> {/* Set gradient background */}
-      <Navbar />
+    <div className="bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen">
+      <Navbar/>
       <RecruitmentPortal />
-      <Footer />
+      <Footer/>
     </div>
   );
 };
